f4fc487ee4f0aae46581aa97695e88b04546918c
Jim Schmatz <jim@siteworx.com>
2015-06-06 02:57:46 -0400

    responsive header

--- static/js/home.js
+++ static/js/home.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	$("#button_search").click(function() {
 		$("header").fadeOut(250, function() {
 			$("header").attr("class", "top col-lg-16");
-			$("header .logo_container").removeClass("col-lg-16").addClass("col-lg-5");
-			$("header .form_container").removeClass("col-lg-16").addClass("col-lg-6");
+			$("header .logo_container").attr("class", "logo_container col-md-8 col-lg-6");
+			$("header .form_container").attr("class", "form_container col-md-8 col-lg-6");
 			
 			$("header").css("top", "-75px");
 			$("header").show();
